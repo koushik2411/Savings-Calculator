dbecbcf57ae57b35aff9ba6b9d218e363c9ed14f
Koushik Kumar <koushikrk1324@gmail.com>
2025-08-24 17:38:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const goalA = document.getElementById("goal");
+const currentA = document.getElementById("current");
+const contributionA = document.getElementById("contribution");
+const calculateBtn = document.getElementById("calcBtn");
+const bar = document.getElementById("progressBar");
+const result = document.getElementById("resultText");
+
+calculateBtn.addEventListener("click", () => {
+    const goalAmount = parseFloat(goalA.value);
+    const currentSave = parseFloat(currentA.value);
+    const mContribution = parseFloat(contributionA.value);
+
+    if (isNaN(goalAmount) || isNaN(currentSave) || isNaN(mContribution)) {
+        alert("Please enter a valid number.")
+    }
+
+    const remainingAmount = goalAmount - currentSave;
+    const monthsToGoal = Math.ceil(remainingAmount/mContribution);
+    const progressPercent = (currentSave/goalAmount)*100;
+
+    bar.style.width = `${progressPercent}%`;
+
+    result.classList.remove("show");
+    setTimeout(() => {
+        if(currentSave>= goalAmount) {
+            result.innerHTML = `"🎉 Congratulations! Your savings have reach your goal";`;
+        } else {
+            result.innerHTML = `🪴 Keep nurturing your savings! You'll reach your goal in ${monthsToGoal} months.`;
+        }
+        result.classList.add("show");
+    }, 100);
+});
